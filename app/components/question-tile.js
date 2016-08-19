8d99754ce5e384a4bd4b7e3e49605ec631d9f7ee
Rachael Bermudes <bermudesorama@gmail.com>
2016-08-19 13:05:15 -0700

    refactored js files

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -6,11 +6,7 @@ export default Ember.Component.extend({
     update(question, params){
       this.sendAction('update', question, params);
     },
-    delete(question) {
-      if (confirm('Are you sure you want to delete this question?')) {
-        this.sendAction('destroyQuestion', question);
-      }
-    }
+    
   }
 
 });
--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-});
+  this.route('question', {path: '/question/:question_id'});
+});   //maybe try content
 
 export default Router;
