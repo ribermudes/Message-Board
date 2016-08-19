4db1f4831077bfd3db37a3b54ff3ae9d2fc316ea
Rachael Bermudes <bermudesorama@gmail.com>
2016-08-19 08:20:21 -0700

    create delete function

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,10 +4,17 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
+
   actions: {
+    saveQuestion(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    },
+
     destroyQuestion(question) {
-      question.destroyQuestion();
-      this.trasitionTo('index');
+      question.destroyRecord();
+      this.transitionTo('index');
     }
   }
 });
